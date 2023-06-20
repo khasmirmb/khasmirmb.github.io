c07cfcc3fbd4413460a19e002477a13498761887
khasmirmb <xt202001178@wmsu.edu.ph>
2023-06-21 01:17:11 +0800

    Updates

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Khasmir - Personal Portfolio</title>
+
+  <!-- Web Fonts -->
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900' type='text/css'>
+
+  <!-- Stylesheet -->
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!-- Custom Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background: #17252A;">
+    <a class="navbar-brand" href="#">My Portfolio</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#hero">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#resume">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#clients">Clients</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+    <!-- Hero section -->
+    <section id="home" class="d-none d-md-block">
+       <div class="hero-wrap">
+        <div class="hero-mask"></div>
+        <div class="hero-bg" style="background-image:url('images/hero.jpg'); filter: blur(5px) brightness(0.5);"></div>
+        <div class="hero-content d-flex flex-column fullscreen py-5">
+			<div class="container my-auto">
+			<div class="row">
+			<div class="col-lg-10 mx-auto text-center">
+              <div class="bg-white shadow rounded-pill p-2 mb-4 d-inline-block wow zoomIn"> <img class="img-fluid rounded-pill d-block profile-small" src="images/profile.jpg" title="I'm Khasmir" alt=""> </div>
+              <h2 class="text-10 text-white text-uppercase fw-600 mb-4 wow fadeInUp">HELLO! I'M AL-KHASMIR - WEB APPLICATION DEVELOPER AND DATABASE MANAGEMENT EXPERT.</h2>
+              <p class="text-5 text-light pb-3 wow fadeInUp">based in Zamboanga City, Philippines.</p>
+              <a href="#about" class="btn btn-primary rounded-0 smooth-scroll wow fadeInUp white-text">About Me</a>
+			</div>
+			</div>
+			</div>
+			<div class="container py-2 text-center"> <a href="#about" class="scroll-down-arrow text-white smooth-scroll"><span class="animated"><i class="fas fa-arrow-down"></i></span></a> </div>
+		</div>
+	  </div>
+    </section>
+
+
+
+  <!-- About section -->
+  <section id="about" class="section">
+    <div class="container">
+      <h2>About Me</h2>
+      <p>Add your content here.</p>
+    </div>
+  </section>
+
+  <!-- Services section -->
+  <section id="services" class="section">
+    <div class="container">
+      <h2>Services</h2>
+      <p>Add your content here.</p>
+    </div>
+  </section>
+
+  <!-- Resume section -->
+  <section id="resume" class="section">
+    <div class="container">
+      <h2>Resume</h2>
+      <p>Add your content here.</p>
+    </div>
+  </section>
+
+  <!-- Projects section -->
+  <section id="projects" class="section">
+    <div class="container">
+      <h2>Projects</h2>
+      <p>Add your content here.</p>
+    </div>
+  </section>
+
+  <!-- Clients section -->
+  <section id="clients" class="section">
+    <div class="container">
+      <h2>Clients</h2>
+      <p>Add your content here.</p>
+    </div>
+  </section>
+
+  <!-- Contact section -->
+  <section id="contact" class="section">
+    <div class="container">
+      <h2>Contact</h2>
+      <p>Add your content here.</p>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      &copy; 2023 Khasmir. All Rights Reserved.
+    </div>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
